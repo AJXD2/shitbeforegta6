0d45e69f74767a0a2621c543250a1f98dc789749
AJXD2 <aj@ajxd2.dev>
2025-01-06 22:01:58 -0500

    Refactor PostType and introduce PostVoteType to enhance type structure and reusability

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,26 +1,34 @@
-import type { SvelteComponent } from "svelte";
 
-export type PostType = {
-		id?: number;
-		user_id: string;
-		title: string;
-		content: string;
-		votes: number;
-		media_url?: string;
-		media_type?: 'image' | 'youtube';
-		created_at?: string;
-	}
+export type BaseEntity = {
+	created_at?: string;
+};
+
+export type PostType = BaseEntity & {
+	id?: number;
+	user_id: string;
+	title: string;
+	content: string;
+	media_url?: string;
+	media_type?: 'image' | 'youtube';
+};
+
+export type PostVoteType = BaseEntity & {
+	id?: number;
+	post_id: string;
+	user_id: string;
+	vote_type: 'up' | 'down';
+};
 
 export type UserType = {
-        id: string;
-        avatar_url: string;
-        full_name: string;
-        updated_at: string;
-}
+	id: string;
+	avatar_url: string;
+	full_name: string;
+	updated_at: string;
+};
 
 export type FlashMessage = {
-    id?: number; 
-    text: string; 
-    type: 'success' | 'error' | 'info' | 'warning'; 
+	id?: number;
+	text: string;
+	type: 'success' | 'error' | 'info' | 'warning';
 	icon?: string;
 };
